3e95a7664c1bfbc8aaba62bbfbf8be10defc1de1
Hirzi Nurfakhrian <hirzi@agate.id>
2019-09-20 10:23:43 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+$(".button-collapse").sideNav();
+
+$(document).ready(function () {
+    $('select').each(function () {
+        $(this).select2({
+            theme: 'bootstrap4',
+            width: 'style',
+            placeholder: 'Select an option',
+            allowClear: Boolean($(this).data('allow-clear')),
+        });
+    });
+    $('.select2-search__field').attr('placeholder', '');
+});
+
+$('select[multiple="multiple"]').on('select2:unselect', function() {
+    $('.select2-search__field').attr('placeholder', '');
+})
+
+const agtDataTableAjax = config => {
+    $(config.element).dataTable({
+        'aaSorting': [],
+        'responsive': true,
+        'lengthChange': false,
+        'aoColumnDefs': [{
+            'bSortable': false,
+            'aTargets': ['sorting_disabled']
+        }],
+        'ajax': config.ajax,
+        'columns': config.columns,
+        'drawCallback': settings => {
+            $('.dataTables_length').addClass('bs-select');
+            $('.dataTables_info').addClass('color-agt-default pl-2');
+            $(config.element + ' thead').addClass('grey-text');
+        },
+    });
+}
